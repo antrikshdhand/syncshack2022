bcffa6cd58995188e6650f94437bad0a2d3934e5
Jack Quinlan <jackquinlan@Jacks-MacBook-Air.local>
2022-08-27 19:43:58 +1000

    Fixed app.py errors

--- app.py
+++ app.py
@@ -3,21 +3,20 @@ from flask import Flask, render_template, redirect, request, url_for, session
 
 def get_db_connection():
 	conn = sqlite3.connect('DB/database.db')
-	conn.row_factory = sqlite.Row
+	conn.row_factory = sqlite3.Row
 	return conn
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "syncshack2022"
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+
+
+@app.route('/app')
 
 def index():
 	conn = get_db_connection()
 	posts = conn.execute('SELECT * FROM posts').fetchall()
 	conn.close()
 	return render_template('index.html', posts=posts)
-
-@app.route('/app', methods=['GET', 'POST'])
-def index():
-    return render_template("app.html")
