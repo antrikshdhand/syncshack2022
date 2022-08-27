0f27501084d00bfbeb70cbd4c80b8d89b8d0111d
Antriksh Dhand <adha5655@uni.sydney.edu.au>
2022-08-28 01:24:13 +1000

    Add Flask sign-up form

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, session, url_for
 from helpers import get_db_connection
+from forms import SignUp
+
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "syncshack2022"
@@ -13,8 +16,13 @@ def index():
 	return render_template('index.html', posts=posts)
 
 @app.route('/signUp', methods=['GET', 'POST'])
-def signup():
-    return render_template("signup.html")
+def signUp():
+    signUpForm = SignUp()
+    if signUpForm.is_submitted():
+        result_dict = request.form.to_dict()
+        session['form1'] = json.dumps(result_dict)
+        return redirect(url_for("index"))
+    return render_template("signup.html", form = signUpForm)
 
 @app.route('/signUp/SignUpSubmit', methods=['GET', 'POST'])
 def signup_click():
