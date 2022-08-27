de6d086079e452af67e09e2a59c4f4ce731d4017
Udit <udit.samant@gmail.com>
2022-08-27 19:37:53 +1000

    Adding gitignore

--- index.py
+++ index.py
@@ -1,11 +1,6 @@
 import sqlite3
 from flask import Flask, render_template, redirect, request, url_for, session
 
-def get_db_connection():
-	conn = sqlite3.connect('DB/database.db')
-	conn.row_factory = sqlite.Row
-	return conn
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "syncshack2022"
 
@@ -18,6 +13,8 @@ def index():
 	conn.close()
 	return render_template('index.html', posts=posts)
 
-@app.route('/app', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template("app.html")
+    return render_template("index.html")
+
+@app.route('/sign')
