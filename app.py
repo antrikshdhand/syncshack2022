379b89309ec95ff90bbdaf0e542a08576c5efdba
Jack Quinlan <jackquinlan@Jacks-MacBook-Air.local>
2022-08-28 00:52:49 +1000

    Added application class

--- app.py
+++ app.py
@@ -4,6 +4,28 @@ from helpers import get_db_connection
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "syncshack2022"
 
+
+class Application:
+
+	def __init__(self, email):
+		self.user_state = 0
+
+		self.email = email #email
+
+		conn = get_db_connection()
+
+		self.user_subjects = () #subject list
+		self.course = "" #course string
+		self.faculty = "" #faculty string
+
+		self.active_users = ()
+		self.invited_users = ()
+		self.accepted_users = ()
+	
+	def preferences(self):
+		pass		
+
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
