7675928e8f6f35297e80464a943960b486bf1791
Jack Quinlan <jackquinlan@Jacks-MacBook-Air.local>
2022-08-28 03:40:59 +1000

    add_user function added

--- helpers.py
+++ helpers.py
@@ -6,3 +6,29 @@ def get_db_connection():
 	conn = sqlite3.connect('DB/database.db')
 	conn.row_factory = sqlite3.Row
 	return conn
+
+
+def add_user(user_attributes):
+	#establish connection
+	
+	connection = get_db_connection()
+	cur = connection.cursor()
+
+	#Validation of user_attributes
+	if user_attributes[:-18] != "@uni.sydney.edu.au":
+		return False
+
+	if user_attributes[2] == '':
+		user_attributes[2] = user_attributes[0]
+	
+
+
+	#adds userdata into the database
+	cur.execute("INSERT INTO users (FirstName, LastName, PrefName, Email, Passw, UserStatus, Course, Faculty) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+	tuple(user_attributes))
+
+	cur.close()
+	return True
+
+	
+	
